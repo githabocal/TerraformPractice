1193bb7189676e250761ca6a6e7a147cbc91e4d3
aocal <A.OCAL88@GMAIL.COM>
2022-11-21 01:34:17 -0500

    aws s3 prevent deletion is added

--- s3.tf
+++ s3.tf
@@ -2,3 +2,28 @@ resource "aws_s3_bucket" "s3" {
   bucket = "s3-bucket-with-terraform"
 
 }
+
+resource "aws_s3_bucket_policy" "prevent_deletion" {
+  bucket = aws_s3_bucket.s3.id
+  policy = data.aws_iam_policy_document.prevent_deletion_policy.json
+}
+
+data "aws_iam_policy_document" "prevent_deletion_policy" {
+  statement {
+    effect = "Deny"
+    principals {
+      type        = "AWS"
+      identifiers = ["123456789012"]
+    }
+    actions = [
+      # "s3:GetObject",
+      # "s3:ListBucket",
+      "s3:DeleteBucket"
+    ]
+
+    resources = [
+      # aws_s3_bucket.arn,
+      # "${aws_s3_bucket.arn}/*"
+    ]
+  }
+}
